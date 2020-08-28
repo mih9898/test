bcf4480722fdf5f1f8186f37293966342eac1c08
Mykhailo Turchanov <mih9898@gmail.com>
2020-08-28 00:19:22 -0500

    lab3

---
+++ src/java112/labs1/LabThree.java
@@ -0,0 +1,31 @@
+package java112.labs1;
+
+/**
+ * The Lab three that continue to
+ * implement TDD principles.
+ */
+public class LabThree {
+
+    /**
+     * Output first argument of the command line
+     *
+     * @param str the first command line string/arg
+     */
+    public void run(String str) {
+        System.out.println("input: " + str);
+    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Please enter one argument on the command line");
+        } else {
+            LabThree lt = new LabThree();
+            lt.run(args[0]);
+        }
+    }
+}
