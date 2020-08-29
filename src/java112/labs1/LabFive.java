ba21b284a6f75bd98f50c536c1df0e3dfb9c8c4b
Mykhailo Turchanov <mih9898@gmail.com>
2020-08-29 00:56:02 -0500

    lab 5 added

--- src/java112/labs1/LabFive.java
+++ src/java112/labs1/LabFive.java
@@ -3,28 +3,30 @@ package java112.labs1;
 import java.io.*;
 
 /**
- * The app reads the text file,
+ * The app write to a text file,
  * and handle checked exceptions
  * with try-catch
  *
  * @author mturchanov
  *
  */
-public class LabFour {
-    /**
+public class LabFive {
+
+     /**
      *
      * Reading a text file
      * and displaying it.
      *
      * @param path the command line argument
-     *   that indicates the path for to read the file
+     *   that indicates the path for file to write
+     *
+     * @param dataToWrite the data to write
      *
      */
-    public void run(String path)  {
-        try( BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)))) {
-            while (br.ready()) {
-                System.out.println(br.readLine());
-            }
+
+    public void run(String path, String dataToWrite)  {
+        try( BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path, true)))) {
+            bw.write(dataToWrite);
         } catch (FileNotFoundException fileNotFoundException) {
             fileNotFoundException.printStackTrace();
         } catch (IOException ioException) {
@@ -36,17 +38,18 @@ public class LabFour {
 
     /**
      * The entry point of application where
-     * we output the text file if appropriate
-     * argument is provided
+     * we write to file if two args are provided
      *
      * @param args the input argument that indicates the path for a file
+     * and data to write
      *
      */
     public static void main(String[] args) {
-        if(args.length != 1) {
-            System.out.println("Please enter one argument on the command line");
+        if(args.length != 2) {
+            System.out.println("Please enter two arguments on the command line, a file name and a message");
         } else {
-            new LabFour().run(args[0]);
+            new LabFive().run(args[0], args[1]);
+            System.out.printf("Data were written to the \"%s\"%n", args[0].replaceAll("^.+\\/", ""));
         }
     }
 }
